b42c6a40f08f0bbe9d538252b6ab7d7f8a6d5e88
bart van egten <b.b.vanegten@gmail.com>
2025-08-01 16:42:46 +0200

    + added music to the background

--- Unity Project/GMTK Game Jam/Assets/Scripts/Audio/AudioManager.cs
+++ Unity Project/GMTK Game Jam/Assets/Scripts/Audio/AudioManager.cs
@@ -1,8 +1,14 @@
 using UnityEngine;
 using FMODUnity;
+using FMOD.Studio;
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager instance { get; private set; }
+
+    public EventReference Music;
+
+    private EventInstance bgMusic;
+
 
     private void Awake()
     {
@@ -14,6 +20,19 @@ public class AudioManager : MonoBehaviour
 
         instance = this;
     }
+
+    private void Start()
+    {
+        bgMusic = createInstance(Music);
+        bgMusic.start();
+    }
+
+    public EventInstance createInstance(EventReference sound) 
+    {
+        return RuntimeManager.CreateInstance(sound);
+
+    }
+
 
     public void PlayOneshot(EventReference sound, Vector3 worldposition) 
     {
