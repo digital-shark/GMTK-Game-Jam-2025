c59adb675c918c83e2248bbe7e4516e9b7933ec2
Victor Ghys <victor.ghys@gmail.com>
2025-08-03 14:17:23 +0200

    Main menu update

--- Unity Project/GMTK Game Jam/Assets/Scripts/MainMenu.cs
+++ Unity Project/GMTK Game Jam/Assets/Scripts/MainMenu.cs
@@ -3,6 +3,17 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject camera;
+    [SerializeField]
+    float rotatingSpeed = 1;
+
+    private void Update()
+    {
+        Quaternion rot = camera.transform.rotation;
+        camera.transform.rotation *= Quaternion.Euler(0, rotatingSpeed * Time.deltaTime, 0);
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene(1);
